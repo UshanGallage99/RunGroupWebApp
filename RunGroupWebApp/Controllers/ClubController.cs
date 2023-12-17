d794c50cc53c609b02d70b110c1b289d3874bb84
Ushan_Gallage <ushansanu@gmail.com>
2023-12-17 10:29:46 +0530

    adding files

---
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+
+        private readonly IClubRepository _clubRepository;
+
+        public ClubController(ApplicationDbContext context, IClubRepository clubRepository)
+        {
+            _clubRepository = clubRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Club> clubs = await _clubRepository.GetAll();
+            return View(clubs);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Club club = await _clubRepository.GetByIdAsync(id);
+            return View(club);      
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var club = await _clubRepository.GetByIdAsync(id);
+            if (club == null) return View("Error");
+            var clubVM = new EditClubViewModel {
+                Title = club.Title, 
+                Description = club.Description, 
+                AddressId = club.AddressId, 
+                Address = club.Address, 
+                URL = club.Image, 
+                ClubCategory = club.ClubCategory };
+            return View(clubVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, EditClubViewModel clubVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit club");
+                return View("Edit", clubVM);
+            }
+
+            var userClub = await _clubRepository.GetByIdAsyncNoTracking(id);
+
+            if (userClub == null)
+            {
+                return View("Error");
+            }
+
+            //var photoResult = await _photoService.AddPhotoAsync(clubVM.Image);
+
+            //if (photoResult.Error != null)
+            //{
+            //    ModelState.AddModelError("Image", "Photo upload failed");
+            //    return View(clubVM);
+            //}
+
+            //if (!string.IsNullOrEmpty(userClub.Image))
+            //{
+            //    _ = _photoService.DeletePhotoAsync(userClub.Image);
+            //}
+
+            var club = new Club
+            {
+                Id = id,
+                Title = clubVM.Title,
+                Description = clubVM.Description,
+                Image = clubVM.Image,
+                AddressId = clubVM.AddressId,
+                Address = clubVM.Address,
+            };
+
+            _clubRepository.Update(club);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ RunGroupWebApp/Repository/RaceRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Data.Enum;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class RaceRepository : IRaceRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public RaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Race race)
+        {
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            _context.Remove(race);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            return await _context.Races.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Race>> GetAllRacesByCity(string city)
+        {
+            return await _context.Races.Where(c => c.Address.City.Contains(city)).Distinct().ToListAsync();
+        }
+
+        public async Task<Race?> GetByIdAsync(int id)
+        {
+            return await _context.Races.Include(i => i.Address).FirstOrDefaultAsync();
+        }
+
+        public Task<Race?> GetByIdAsyncNoTracking(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountByCategoryAsync(RaceCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Race>> GetRacesByCategoryAndSliceAsync(RaceCategory category, int offset, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Race>> GetSliceAsync(int offset, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
